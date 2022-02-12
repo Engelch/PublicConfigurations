67bfe56f59d2b62c1e2632fc3f1afb529ffe12e6
Christian Engel <engel-ch@outlook.com>
2022-02-12 17:46:35 +0100

    version information added to docker/build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,5 +3,9 @@
 # dry-run 
 [ "$1" = -n ] && dry=echo && echo DRY RUN ONLY.
 name=$(basename $PWD | sed -e 's/ /_/g' | tr [A-Z] [a-z])
-echo Building image $name...
-$dry docker build -t $name .
+if [ -r .version ] ; then
+    version=":"$(cat .version | grep -v '^$')
+    echo version is $version
+fi
+echo Building image $name$version...
+$dry docker build -t $name$version .
