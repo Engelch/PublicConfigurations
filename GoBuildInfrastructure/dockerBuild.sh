ec43d305ea359331c98f28d16179a18eeeb5a59c
Christian Engel <engel-ch@outlook.com>
2022-02-14 00:24:55 +0100

    Docker/build.sh now added additionally as GoBuildInfrastructure/dockerBuild.sh

---
+++ GoBuildInfrastructure/dockerBuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# dry-run 
+[ "$1" = -n ] && dry=echo && echo DRY RUN ONLY.
+name=$(basename $PWD | sed -e 's/ /_/g' | tr [A-Z] [a-z])
+if [ -r .version ] ; then
+    version=":"$(cat .version | grep -v '^$' | sed 's/[[:space:]]*#.*$//')
+    echo version is $version
+fi
+echo Building image $name$version...
+$dry docker build -t $name$version -t $name -t $name:latest .
