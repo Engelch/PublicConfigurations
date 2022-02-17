a9e74ed951b4b8226608044ea3220158f3d0fed8
Christian Engel <engel-ch@outlook.com>
2022-02-17 23:10:03 +0100

    dockerBuild alignment with dockerStart

--- GoBuildInfrastructure/dockerBuild.sh
+++ GoBuildInfrastructure/dockerBuild.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+VERSION=0.2.0
+
 # dry-run 
-[ "$1" = -n ] && dry=echo && echo DRY RUN ONLY.
-name=$(basename $PWD | sed -e 's/ /_/g' | tr [A-Z] [a-z])
+[ "$1" = -n ] && dry=echo && echo DRY RUN ONLY........................
+
+for file in _container_* ; do
+  [ $file = '_container_*'  ] && continue
+  container=$(echo $file | sed -e "s/_container_//" | sed -e "s,_,/,g" ) 
+done
+if [ -z $container ] ; then
+  container=$(basename $PWD | sed -e 's/ /_/g' | tr [A-Z] [a-z]) # as in dockerBuild
+fi
+
 if [ -r .version ] ; then
     version=":"$(cat .version | grep -v '^$' | sed 's/[[:space:]]*#.*$//')
     echo version is $version
+    ver="-t $name$version"
 fi
-echo Building image $name$version...
-$dry docker build -t $name$version -t $name -t $name:latest .
+echo Building image "[$VERSION]" $ver $name $name:latest...
+$dry docker build $ver -t $name -t $name:latest .
+
+# EOF
