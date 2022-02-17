7e7caaef1bba6920153f1b637d9ce9aea0fba02c
Christian Engel <engel-ch@outlook.com>
2022-02-17 23:15:37 +0100

    fix dockerstart kill

--- GoBuildInfrastructure/dockerStart.sh
+++ GoBuildInfrastructure/dockerStart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.2.2
+VERSION=0.2.3
 
 function err()          { echo $* 1>&2; } # just write to stderr
 
@@ -70,8 +70,8 @@ debug Container is $container
 debug sudo is set to $sudo
 
 if [ "$1" = -k ] ; then
-  debug docker kill $( echo $name | awk '{ print $2 }' )
-  $dry docker kill $( echo $name | awk '{ print $2 }' )
+  debug docker kill $( echo $container | awk '{ print $2 }' )
+  $dry docker kill $( echo $container | awk '{ print $2 }' )
   res=$?
 else
   debug $sudo docker run $rm  $net $name $map $dirmap $* $container 
