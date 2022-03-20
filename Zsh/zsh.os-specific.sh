bd58f3bc244c4486b7a772a0303a44c4fd40cca2
Christian Engel <engel-ch@outlook.com>
2022-03-20 02:41:27 +0100

    Added a function docker() for OSX on M1 so that no warnings are created when starting an amd64 docker container.

--- Zsh/zsh.os-specific.sh
+++ Zsh/zsh.os-specific.sh
@@ -74,6 +74,16 @@ function setupOSX() {
       which $_newApp | egrep '^/opt/homebrew|/usr/local' > /dev/null || warning $_newApp does not seem to be from homebrew.
    done
 
+  # useful only for Mac OS Silicon M1, 
+  # still working but useless for the other platforms
+  docker() {
+   if [[ `uname -m` == "arm64" ]] && [[ "$1" == "run" || "$1" == "build" ]]; then
+      /usr/local/bin/docker "$1" --platform linux/amd64 "${@:2}"
+    else
+       /usr/local/bin/docker "$@"
+    fi
+  }
+
    pkgU() # upgrade homebrew, gcloud,  macTeX
    {
       echo homebrew........................................  1>&2
--- Zsh/zsh.version.sh
+++ Zsh/zsh.version.sh
@@ -12,7 +12,7 @@ function zshSetVersion() {
    # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
    # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
    # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-   export ZSH_RC_VERSION="2.5.1"
+   export ZSH_RC_VERSION="2.5.2"
    debug ZSH_RC_VERSION is $ZSH_RC_VERSION
    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
